30f616b7ca8cf7a0f1bcaded181055d74eeec2b4
Arthur Bugarin <arthur.mello.marques@gmail.com>
2025-09-09 16:55:53 -0300

    add rtw89 driver to image

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,31 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+
+# install rtw89 driver
+dnf5 install -y dkms
+dnf5 install -y kernel-devel
+dnf5 install -y gcc
+dnf5 install -y make
+dnf5 install -y git
+
+git clone https://github.com/morrownr/rtw89 /tmp/rtw89
+cd /tmp/rtw89
+dkms install $PWD \
+  --kernelsourcedir /usr/src/kernels/6.16.4-107.bazzite.fc42.x86_64 \
+  -k 6.16.4-107.bazzite.fc42.x86_64
+#make clean modules
+#make install
+make install_fw
+cp -v rtw89.conf /etc/modprobe.d/
+if mokutil --sb-state >/dev/null 2>&1; then
+    echo "Secure Boot detected, trying to import MOK key..."
+    mokutil --import /var/lib/dkms/mok.pub || echo "Failed to import MOK key (probably expected in container)"
+else
+    echo "Skipping MOK import: no EFI support (container build)"
+fi
+
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
